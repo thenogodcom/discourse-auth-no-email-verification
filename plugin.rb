b90c347148326101095280b2386a6700dcfa2d65
thenogodcom <thenogodcom@gmail.com>
2025-03-21 22:30:52 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -7,13 +7,15 @@
 after_initialize do
   if SiteSetting.auth_no_email_enabled
     on(:user_created) do |user|
-      user.update(active: true)
-      user.update(approved: true) if !SiteSetting.must_approve_users?
+      user.active = true
+      user.approved = true if !SiteSetting.must_approve_users?
+      user.save! # <--- Reverted to save!
 
       if user.staged
           EmailToken.where(user_id: user.id).update_all(confirmed: true)
       elsif token = EmailToken.find_by(email: user.email)
-          token.update(confirmed: true)
+          token.confirmed = true
+          token.save! # <--- Reverted
       end
     end
   end
